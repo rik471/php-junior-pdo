7366ef553ed4a56ae73a7618503d9a61c9957b41
rik471 <riksongleison471@gmail.com>
2019-01-22 01:16:08 -0200

    importing pdo project

--- classes/config.php
+++ classes/config.php
@@ -6,4 +6,4 @@ define('DB_DRIVE', 'mysql');
 define('DB_HOSTNAME', '127.0.0.1');
 define('DB_DATABASE', 'php_pdo');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'darkside');
+define('DB_PASSWORD', 'root');
